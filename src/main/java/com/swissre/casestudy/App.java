743c441680994567d75462c29cbe20c199bd070a
G K Chaitanya <chaitu.goteti@gmail.com>
2024-12-02 02:31:37 +0530

    case study

--- src/main/java/com/swissre/casestudy/App.java
+++ src/main/java/com/swissre/casestudy/App.java
@@ -1,13 +1,44 @@
 package com.swissre.casestudy;
 
+import com.swissre.casestudy.exceptions.CaseStudyException;
+import com.swissre.casestudy.models.Employee;
+import com.swissre.casestudy.models.ManagerReportees;
+import com.swissre.casestudy.report.OrgReport;
+import com.swissre.casestudy.utils.FileUtils;
+import com.swissre.casestudy.utils.ReportingUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        FileUtils fileUtils = new FileUtils();
+        Map<Integer, Employee> employeeMap = null;
+        try {
+            employeeMap = fileUtils.getEmployees();
+        } catch (IOException e) {
+            System.out.println("IOException while fetching employees from "+e.getMessage());
+        } catch (CaseStudyException e) {
+            System.out.println(e.getMessage());
+        }
+
+        if(employeeMap == null || employeeMap.isEmpty()){
+            System.out.println("No employee found exiting..");
+            return;
+        }
+        // get manager and the subordinates list, subordinates avg sal
+        ManagerReportees managers = ReportingUtil.formEmployeeHierarchy(employeeMap);
+
+        OrgReport orgReport = new OrgReport(FileUtils.minSalPercentage, FileUtils.maxSalPercentage, FileUtils.minLevels);
+        orgReport.printReport(employeeMap, managers.getSubordinatesAvgSal());
+
+        System.out.println(employeeMap);
+        System.out.println("CEO Manager Id: "+ employeeMap.values().stream().filter( e -> e.getManagerId() == -1).findFirst().get());
     }
 }
--- src/test/java/com/swissre/casestudy/AppTest.java
+++ src/test/java/com/swissre/casestudy/AppTest.java
@@ -1,8 +1,15 @@
 package com.swissre.casestudy;
 
+import com.swissre.casestudy.exceptions.CaseStudyException;
+import com.swissre.casestudy.models.Employee;
+import com.swissre.casestudy.utils.FileUtils;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Unit test for simple App.
@@ -10,6 +17,8 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
+
+    App app = new App();
     /**
      * Create the test case
      *
@@ -31,8 +40,8 @@ public class AppTest
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() throws IOException, CaseStudyException {
+        Map<Integer, Employee> employees = new FileUtils().getEmployees();
+        assertEquals(employees.size(), 9);
     }
 }
